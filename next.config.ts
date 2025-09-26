f6f9fa963e44ea5d2c10e308ceb5a0d63c41e0f5
«Elena <LenaRybinskova@gmail.com>
2025-09-26 17:48:33 +0300

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true
+    },
+    basePath: process.env.NODE_ENV === 'production' ? '/my-nextjs-app' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/my-nextjs-app/' : '',
+}
 
-export default nextConfig;
+module.exports = nextConfig
