cf20a7061438f90b1ef84604edd37ffa0a0466bd
«Elena <LenaRybinskova@gmail.com>
2025-09-30 23:35:03 +0300

    fix basePath:

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,3 @@
-/*
-import type { NextConfig } from "next";
-
-const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
-    images: {
-        unoptimized: true
-    },
-    basePath: process.env.NODE_ENV === 'production' ? '/my-nextjs-app' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/my-nextjs-app/' : '',
-}
-
-module.exports = nextConfig*/
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     trailingSlash: true,
